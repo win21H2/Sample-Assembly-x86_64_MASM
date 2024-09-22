d53a022fb93000d81f5d87e84ee24d2867ee3668
9662e103-129a <92825997+win21H2@users.noreply.github.com>
2024-09-21 20:48:23 -0500

    Refactor x86 assembly code for clarity and functionality
    Updated comment header to include "x86_64 INTEL | WASM (x86 runtime)" and a signed identifier. Replaced `.386` and `.model flat,stdcall` with `.386` and `.MODEL flat, stdcall`. Removed `ExitProcess PROTO, dwExitCode:DWORD` and `sum DWORD ?` declaration. Changed `.data` to `.DATA` and added `INPUT_MSG`, `INVALID_MSG`, and `INPUT_BUFFER`. Changed `.code` to `.CODE` and replaced `main proc` with `start` label. Included `kernel32.inc` and `masm32.inc` headers and linked libraries. Replaced `main endp` and `end main` with `end start`. Added user input handling for `sum`, `difference`, `product`, `quotient`, and `solve` with appropriate messages and exits. Replaced `invoke ExitProcess, 0` with conditional jumps and `ExitProcess` calls based on user input.

--- Main.asm
+++ Main.asm
@@ -1,21 +1,75 @@
-; x86_64 with WASM compiler
+; x86_64 INTEL | WASM (x86 runtime)
+; Signed, 9662e103-129a
 
-.386 ; sets the processor to anything newer than a 386 processor
-.model flat,stdcall
-.stack 4096 ; allocates 4096 bytes to the program stack
-ExitProcess PROTO, dwExitCode:DWORD
+; http://www.c-jump.com/CIS77/ASM/Instructions/lecture.html
 
-.data
+; +----------+--------------------------------------------------------+
+; | Register | Function                                               |
+; +----------+--------------------------------------------------------+
+; | eax      | Accumulator for operands and results                   |
+; +----------+--------------------------------------------------------+
+; | ebx      | Base pointer to data in the data segment               |
+; +----------+--------------------------------------------------------+
+; | ecx      | Counter for loop operations                            |
+; +----------+--------------------------------------------------------+
+; | edx      | I/O pointers                                           |
+; +----------+--------------------------------------------------------+
+; | ebp      | Frame pointer - useful for stack frames                |
+; +----------+--------------------------------------------------------+
+; | esp      | Stack pointer - hardcoded into PUSH and POP operations |
+; +----------+--------------------------------------------------------+
+; | esi      | Source index - required for some array operations      |
+; +----------+--------------------------------------------------------+
+; | edi      | Destination index - required for some array operations |
+; +----------+--------------------------------------------------------+
+; | eip      | Instruction pointer                                    |
+; +----------+--------------------------------------------------------+
+; | eflags   | Result flags - hardcoded into conditional operations   |
+; +----------+--------------------------------------------------------+
 
-sum DWORD ?
+; ExitProcess code [1]: regular solve method
+; ExitProcess code [2]: solve function method
 
-.code 
-main proc
-	mov eax, 7 ; sets address eax to '7'
-	add eax, 4 ; adds '4' to the address in eax
-	mov sum, eax ; moves the contents of the address eax into the address 'sum'
+.386
+.MODEL flat, stdcall
+option casemap:none
 
-	invoke ExitProcess, 0 ; invokes the end of the program
+include \masm32\include\kernel32.inc
+include \masm32\include\masm32.inc
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
 
-main endp
-end main
+.DATA
+    INPUT_MSG db "Input: [s]um, [d]ifference, [p]roduct, [q]uotient, or s[o]lve: ", 0
+    INVALID_MSG db "Invalid!", 0Ah, 0
+    INPUT_BUFFER db 2 dup(0)
+
+.CODE
+start:
+    INVOKE StdOut, addr INPUT_MSG
+    INVOKE StdIn, addr INPUT_BUFFER, sizeof INPUT_BUFFER
+
+    MOV al, [INPUT_BUFFER]
+    
+    CMP al, 'o'
+    JE solve
+    CMP al, 's'
+    JE valid
+    CMP al, 'd'
+    JE valid
+    CMP al, 'p'
+    JE valid
+    CMP al, 'q'
+    JE valid
+
+invalid:
+    INVOKE StdOut, addr INVALID_MSG
+    JMP start
+
+valid:
+    INVOKE ExitProcess, 1
+
+solve:
+    INVOKE ExitProcess, 2
+
+end start
